6ee744dd7d7118a8928a2a1c96dae2eb0331ac68
Timofey Tipishev <tipishev@gmail.com>
2020-05-08 17:23:03 +0200

    add a sensor example

--- airflow/dags/test_operators.py
+++ airflow/dags/test_operators.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import MyFirstOperator
+from airflow.operators import MyFirstOperator, MyFirstSensor
 
-dag = DAG('my_test_dag', description='Another tutorial DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+with DAG('my_test_dag', description='Another tutorial DAG',
+         schedule_interval='0 12 * * *',
+         start_date=datetime(2017, 3, 20), catchup=False) as dag:
 
-dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+    dummy_task = DummyOperator(task_id='dummy_task')
 
-operator_task = MyFirstOperator(my_operator_param='This is a test.',
-                                task_id='my_first_operator_task', dag=dag)
+    sensor_task = MyFirstSensor(task_id='my_sensor_task', poke_interval=30)
 
-dummy_task >> operator_task
+    operator_task = MyFirstOperator(my_operator_param='This is a test.',
+                                    task_id='my_first_operator_task')
+
+dummy_task >> sensor_task >> operator_task
